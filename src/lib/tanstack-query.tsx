6c5f6ee5f1c7f2d6ef38ac4a48254793d459ecb5
eungi <deveungi@gmail.com>
2025-03-28 10:13:13 +0900

    chore: configuration tanstack-query (#3)

---
+++ src/lib/tanstack-query.tsx
@@ -0,0 +1,32 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode } from 'react';
+
+interface QueryProviderProps {
+  children: ReactNode;
+}
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+      retry: 1,
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
+    },
+    mutations: {
+      retry: 1,
+    },
+  },
+});
+
+
+export function TanStackQueryProvider({ children }: QueryProviderProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { TanStackQueryProvider } from './lib/tanstack-query'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TanStackQueryProvider>
+      <App />
+    </TanStackQueryProvider>
   </StrictMode>,
 )
