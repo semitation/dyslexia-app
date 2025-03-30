86265654d69a5dd57eef46fdf0945ecb9ba97ee8
ChiyunSong <heychi0728@gmail.com>
2025-03-30 16:39:19 +0900

    feat: 랜딩페이지 문의영역 추가

--- src/page/landing/index.tsx
+++ src/page/landing/index.tsx
@@ -1,2 +1,3 @@
 export { default as Hero } from './hero';
 export { default as News } from './news';
+export { default as Contact } from './contact';
--- src/page/landing/news/index.tsx
+++ src/page/landing/news/index.tsx
@@ -3,32 +3,32 @@ import { newsData } from "./data";
 
 export default function News() {
   return (
-    <section className="w-full pt-32 flex flex-col justify-center gap-y-6 items-center bg-white">
+    <section className="w-full flex flex-col justify-center gap-y-6 items-center bg-white">
       <Typography as="h2" variant="h2" weight="bold" color="primary" className="text-3xl mb-8 leading-8">
-      모든 학생은 배울 권리가 있습니다.
+        모든 학생은 배울 권리가 있습니다.
       </Typography>
 
 
       <nav className="flex flex-col gap-y-3">
-      {newsData.map(news => (
-        <Card className="w-full flex" key={news.title}>
-        <CardContent className="flex items-end justify-between">
-          <section className="flex flex-col gap-y-1">
-          <Typography as="h3" variant="h3" weight="bold" className="text-xl text-slate-600">
-            {news.title}
-          </Typography>
-          <Typography as="p" variant="p" className="text-md">
-            {news.description}
-          </Typography>
-          </section>
+        {newsData.map(news => (
+          <Card className="w-full flex" key={news.title}>
+            <CardContent className="flex items-end justify-between">
+              <section className="flex flex-col gap-y-1">
+                <Typography as="h3" variant="h3" weight="bold" className="text-xl text-slate-600">
+                  {news.title}
+                </Typography>
+                <Typography as="p" variant="p" className="text-md">
+                  {news.description}
+                </Typography>
+              </section>
 
-          <Typography variant="p" className="text-md text-slate-800">
-            {news.year}년 국내기사
-          </Typography>
+              <Typography variant="p" className="text-md text-slate-800">
+                {news.year}년 국내기사
+              </Typography>
 
-        </CardContent>
-    </Card>
-      ))}
+            </CardContent>
+          </Card>
+        ))}
       </nav>
     </section>
   );
