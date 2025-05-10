f574acd77832c336c01ed18a7ef715b63d9ecd4c
deveungi <deveungi@gmail.com>
2025-05-10 14:01:40 +0900

    feat: 인증 API 및 로컬 스토리지 유틸리티 추가

---
+++ src/shared/utils/storage.ts
@@ -0,0 +1,15 @@
+const STORAGE_KEYS = {
+  SIGNUP_CLIENT_ID: 'signup_client_id',
+} as const
+
+export const storage = {
+  getClientId: () => {
+    return localStorage.getItem(STORAGE_KEYS.SIGNUP_CLIENT_ID)
+  },
+  setClientId: (clientId: string) => {
+    localStorage.setItem(STORAGE_KEYS.SIGNUP_CLIENT_ID, clientId)
+  },
+  removeClientId: () => {
+    localStorage.removeItem(STORAGE_KEYS.SIGNUP_CLIENT_ID)
+  },
+} 
