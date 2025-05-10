14847ce115de41e16216d82e92f57ffb7edcf3de
deveungi <deveungi@gmail.com>
2025-05-10 14:02:20 +0900

    feat: 기본 레이아웃 및 헤더 컴포넌트 추가

--- src/features/layouts/header/index.tsx
+++ src/features/layouts/header/index.tsx
@@ -1,7 +1,19 @@
 import { Button } from "@/shared/ui/button";
-import { Typography } from "@/shared/ui";
+import { Show, Typography } from "@/shared/ui";
+import { useAuth } from "@/shared/hooks/use-auth";
 
-export default function Header() {
+
+type HeaderProps = {
+  onAuthClick: () => void
+}
+
+export default function Header({ onAuthClick }: HeaderProps) {
+  const { isAuthenticated, my, logout } = useAuth();
+
+  const onClickLogout = () => {
+    logout();
+  }
+
   return (
     <header className="w-full min-h-[64px] flex justify-center">
       <section className="max-w-[768px] w-full flex items-center justify-between">
@@ -9,10 +21,34 @@ export default function Header() {
           리딩브릿지
           </Typography>
 
-        <nav className="flex gap-2">
-          <Button variant="default">로그인</Button>
-          <Button variant="outline">회원가입</Button>
+        <Show when={!isAuthenticated}>
+          <nav className="flex gap-2">
+            <Button variant="default" onClick={onAuthClick}>로그인</Button>
+            <Button variant="outline" onClick={onAuthClick}>회원가입</Button>
+          </nav>
+        </Show>
+
+        <Show when={isAuthenticated}>
+        <nav className="flex gap-2 items-center">
+          <Typography variant="p" color="destructive">
+            안녕하세요,
+          </Typography>
+          <Typography variant="p" color="primary">
+            {my?.name}
+          </Typography>
+          <Typography variant="p" color="destructive">
+            님
+          </Typography>
+
+          <Button 
+            variant="default" 
+            onClick={onClickLogout}
+            className="ml-2"
+          >
+              로그아웃
+          </Button>
         </nav>
+        </Show>
 
       </section>
     </header>
